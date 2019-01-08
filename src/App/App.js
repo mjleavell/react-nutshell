7e74ae1a5e85a4440091ed3f1ed5d77528ec5497
Maggie Leavell <mjleavell@gmail.com>
2019-01-07 20:31:06 -0600

    login and logout working

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,60 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import connection from '../helpers/data/connection';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+import Auth from '../components/Auth/Auth';
 import './App.scss';
+import authRequests from '../helpers/data/authRequests';
 
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    connection();
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+        });
+      } else {
+        this.setState({
+          authed: false,
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  }
+
   render() {
+    const { authed } = this.state;
+
+    const logoutClickEvent = () => {
+      authRequests.logoutUser();
+      this.setState({ authed: false });
+    };
+
+    if (!authed) {
+      return (
+        <div className="App">
+          <MyNavbar isAuthed={authed} logoutClickEvent={logoutClickEvent} />
+          <Auth isAuthenticated={this.isAuthenticated} />
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <button className='btn btn-light'>bootstrap</button>
-        <Button
-          tag='a'
-          color='dark'
-          href="http://reactstrap.github.io"
-          target="_blank"
-        >Click to Docs</Button>
+        <MyNavbar isAuthed={authed} logoutClickEvent={logoutClickEvent} />
+        you are authenticated
       </div>
     );
   }
